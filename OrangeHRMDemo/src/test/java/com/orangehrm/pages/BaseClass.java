e593c099f8da07ece8390d1a87819555c704e5c2
ashok <ashokp@prospectatech.com>
2019-07-26 07:28:04 +0530

    Maven build and parameter passing

--- OrangeHRMDemo/src/test/java/com/orangehrm/pages/BaseClass.java
+++ OrangeHRMDemo/src/test/java/com/orangehrm/pages/BaseClass.java
@@ -12,6 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -41,10 +42,12 @@ public class BaseClass
 		report.attachReporter(extent);
 	}
 	
+	@Parameters({"browser","urlToBeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String browser,String url)
 	{
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getUrl());
+		//driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getUrl());
+		driver = BrowserFactory.startApplication(driver, browser, url);
 	}
 	
 	@AfterMethod
